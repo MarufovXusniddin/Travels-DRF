61ff3b822bffbccf0fb845e296ec978bf17dc1db
Xusniddin <kursantmarufov@gmail.com>
2024-06-01 20:24:23 +0500

    second commit

--- travel/app/urls.py
+++ travel/app/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from .views import TravelAPI, HotelAPI, KlassAPI
+from .views import TravelAPI, TravelDetailAPI, HotelAPI, HotelDetailAPI, KlassAPI, KlassDetailAPI
 
 urlpatterns = [
     path('api/v1/travels/', TravelAPI.as_view()),
-    path('api/v1/travel/<int:pk>/', TravelAPI.as_view()),
+    path('api/v1/travel/<int:pk>/', TravelDetailAPI.as_view()),
 
     path('api/v1/hotels/', HotelAPI.as_view()),
-    path('api/v1/hotel/<int:pk>/', HotelAPI.as_view()),
+    path('api/v1/hotel/<int:pk>/', HotelDetailAPI.as_view()),
 
     path('api/v1/klasses/', KlassAPI.as_view()),
-    path('api/v1/klass/<int:pk>/', KlassAPI.as_view())
+    path('api/v1/klass/<int:pk>/', KlassDetailAPI.as_view())
 ]
+
+
+# Hech kimdan ko'chirganim yo'q doim o'zm bajaraman
--- travel/app/views.py
+++ travel/app/views.py
@@ -1,134 +1,98 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import mixins
+from rest_framework import generics
 from .models import Travel, Klass, Hotel
 from .serializers import TravelSerializer, KlassSerializer, HotelSerializer
+
 
 # Create your views here.
 
 
-class TravelAPI(APIView):
-    def get(self, request, pk=None):
-        if pk is None:
-            travels = Travel.objects.all()
-            return Response({'travels': TravelSerializer(travels, many=Travel).data})
-        else:
-            try:
-                travel = Travel.objects.get(pk=pk)
-                return Response({'travel': TravelSerializer(travel).data})
-            except:
-                return Response({"error": "Bu id da maqola mavjud emas!"})
+class TravelAPI(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    queryset = Travel.objects.all()
+    serializer_class = TravelSerializer
 
-    def post(self, request):
-        serializer = TravelSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        travels = serializer.save()
-        return Response({'travels': TravelSerializer(travels).data})
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def put(self, request, pk=None):
-        if pk is None:
-            return Response({"detail": "Method \"PUT\" not allowed."})
-        try:
-            travels = Travel.objects.get(pk=pk)
-            serializer = TravelSerializer(travels, data=request.data)
-            serializer.is_valid(raise_exception=True)
-            updated_travel = serializer.save()
-            return Response({'news': TravelSerializer(updated_travel).data})
-        except:
-            return Response({'error': "Bu id da maqola mavjud emas"})
-
-    def delete(self, request, pk=None):
-        if pk is None:
-            return Response({"detail": "Method \"DELETE\" not allowed."})
-        try:
-            travel = Travel.objects.get(pk=pk)
-            travel.delete()
-            return Response({'success': "Maqola o'chirildi"})
-        except:
-            return Response({'error': "Bu id da maqola mavjud emas"})
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class HotelAPI(APIView):
-    def get(self, request, pk=None):
-        if pk is None:
-            hotels = Hotel.objects.all()
-            return Response({"hotels": HotelSerializer(hotels, many=True).data})
-        else:
-            try:
-                hotel = Hotel.objects.get(pk=pk)
-                return Response({"hotel": HotelSerializer(hotel).data})
-            except:
-                return Response({"error": "Bu id da maqola mavjud emas!"})
+class TravelDetailAPI(generics.GenericAPIView, mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.DestroyModelMixin):
+    queryset = Travel.objects.all()
+    serializer_class = TravelSerializer
 
-    def post(self, request):
-        serializer = HotelSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        hotel = serializer.save()
-        return Response({"hotel": HotelSerializer(hotel).data})
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk=None):
-        if pk is None:
-            return Response({"detail": "Method \"PUT\" not allowed."})
-        else:
-            try:
-                hotel = Hotel.objects.get(pk=pk)
-                serializer = HotelSerializer(hotel, data=request.data)
-                serializer.is_valid()
-                updated_hotel = serializer.save()
-                return Response({"hotel": HotelSerializer(updated_hotel).data})
-            except:
-                return Response({"error": "Bu id da maqola mavjud emas!"})
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def delete(self, request, pk=None):
-        if pk is None:
-            return Response({"detail": "Method \"DELETE\" not allowed."})
-        else:
-            try:
-                hotel = Hotel.objects.get(pk=pk)
-                hotel.delete()
-                return Response({"success": "Maqola o'chirildi !"})
-            except:
-                return Response({"Bu id da maqola mavjud emas!"})
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
-class KlassAPI(APIView):
-    def get(self, request, pk=None):
-        if pk is None:
-            klasses = Klass.objects.all()
-            return Response({"klasses": HotelSerializer(klasses, many=True).data})
-        else:
-            try:
-                klass = Klass.objects.get(pk=pk)
-                return Response({"klass": HotelSerializer(klass).data})
-            except:
-                return Response({"error": "Bu id da maqola mavjud emas!"})
+class HotelAPI(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
 
-    def post(self, request):
-        serializer = KlassSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        klass = serializer.save()
-        return Response({"hotel": HotelSerializer(klass).data})
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def put(self, request, pk=None):
-        if pk is None:
-            return Response({"detail": "Method \"PUT\" not allowed."})
-        else:
-            try:
-                klass = Klass.objects.get(pk=pk)
-                serializer = HotelSerializer(klass, data=request.data)
-                serializer.is_valid()
-                updated_klass = serializer.save()
-                return Response({"hotel": HotelSerializer(updated_klass).data})
-            except:
-                return Response({"error": "Bu id da maqola mavjud emas!"})
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-    def delete(self, request, pk=None):
-        if pk is None:
-            return Response({"detail": "Method \"DELETE\" not allowed."})
-        else:
-            try:
-                klass = Klass.objects.get(pk=pk)
-                klass.delete()
-                return Response({"success": "Maqola o'chirildi !"})
-            except:
-                return Response({"Bu id da maqola mavjud emas!"})
+
+class HotelDetailAPI(generics.GenericAPIView, mixins.RetrieveModelMixin,
+                                              mixins.UpdateModelMixin,
+                                              mixins.DestroyModelMixin):
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class KlassAPI(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    queryset = Klass.objects.all()
+    serializer_class = KlassSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class KlassDetailAPI(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    queryset = Klass.objects.all()
+    serializer_class = KlassSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
